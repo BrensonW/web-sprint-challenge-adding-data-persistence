386e522b054ece327a3ab07d5e3c68fa4f2d5f93
Brenson <brensonw300@gmail.com>
2020-11-06 09:08:40 -0800

    Looked through code and made sure it was all good and now MVP is done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const helmet = require('helmet')
-const projectsRouter = require('./routers/project-router')
+const express = require("express")
+const helmet = require("helmet")
+const projectsRouter = require("./routers/project-router")
 
-const server = express();
-const port = process.env.PORT || 3000
+const server = express()
+const port = process.env.PORT || 4000
 
 server.use(helmet())
 server.use(express.json())
@@ -11,12 +11,12 @@ server.use(express.json())
 server.use(projectsRouter)
 
 server.use((err, req, res, next) => {
-    console.log(err)
-    res.status(500).json({
-        message: 'Something Went Wrong',
-    })
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
 })
 
 server.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
+	console.log(`Running at http://localhost:${port}`)
 })
